95475eae1e63b7fe50ac64eeb89377707488fb57
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-03-18 14:11:44 -0700

    add boilerplate to main components

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import './App.scss';
+import NavBar from './NavBar';
+// import Scene from './Scene';
+import AboutMe from './AboutMe';
+import Projects from './Projects';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <AboutMe />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
