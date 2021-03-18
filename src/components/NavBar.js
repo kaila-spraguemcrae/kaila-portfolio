8aae9d82b93fc5639cd055eebb0b174024475058
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-03-18 14:52:57 -0700

    update NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav mb-2 mb-lg-0">
             <li className="nav-item">
-             
+              
             </li>
             <li className="nav-item">
               
