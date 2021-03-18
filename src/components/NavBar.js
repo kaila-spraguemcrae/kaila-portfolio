ea4463f68dc12ef24427e3c13c169ae0ac39faff
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-03-18 14:33:57 -0700

    add NavBar bones and import react-scroll

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const NavBar = () => {
-
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-white bg-white">
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+             
+            </li>
+            <li className="nav-item">
+              
+            </li>
+            <li className="nav-item">
+              
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default NavBar;
