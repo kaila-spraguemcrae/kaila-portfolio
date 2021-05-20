aeb03f89bedb70a1f4c71aff0bc16f269b44e96b
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-05-20 12:01:42 -0700

    add source to footer and update font

--- src/components/FooterSection/index.js
+++ src/components/FooterSection/index.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import { FooterText } from './FooterElements';
+import React from "react";
+import { FooterText, Source } from "./FooterElements";
 
 const Footer = () => {
   return (
     <div>
-      <FooterText>Kaila Sprague McRae | source</FooterText>
+      <FooterText>
+        Kaila Sprague McRae |{" "}
+        <Source
+          href="https://github.com/kaila-spraguemcrae/kaila-portfolio"
+          target="_blank"
+        >
+          source
+        </Source>
+      </FooterText>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
